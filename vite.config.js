a40fa0c12d91be6342ecc196768cf209831e18c0
gedion <legessegedion@gmail.com>
2025-04-13 18:32:54 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Apple-React",
   plugins: [react()],
-  server: {
+
+  /*server: {
     port: 4041,
   },
   css: {
     devSourcemap: false,
-  },
+  },*/
 });
